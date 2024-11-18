8042dacbb1ac00d263fa78eeb4cf8963f00e3988
Sainath <sainathdora4@gmail.com>
2024-11-18 21:34:42 +0530

    Added GenAi

--- Q2.java
+++ Q2.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Q2 {
     public static void main(String[] args) {
         ArrayList<String> Fruits = new ArrayList<>();
-        Fruits.add("Mango");
+        Fruits.add("Mango"); 
         Fruits.add("Banana");
         Fruits.add("Apple");
         System.out.println(Fruits);
